1bc5566ac53765053f7de80c1621ee9069b7f734
connection0320 <100455847+good0neighbour@users.noreply.github.com>
2022-08-27 18:38:38 +0900

    ShootingFighter
    Complete single shooting fighter game

---
+++ ShootingFighter/Assets/02.Scripts/PlayerMove.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 3.0f;
+    private float _h => Input.GetAxisRaw("Horizontal");
+    private float _v => Input.GetAxisRaw("Vertical");
+    private void FixedUpdate()
+    {
+        Vector3 directionVector = new Vector3(_h, 0, _v).normalized;
+        Vector3 deltamove = directionVector * _moveSpeed * Time.fixedDeltaTime;
+        transform.Translate(deltamove);
+    }
+}
