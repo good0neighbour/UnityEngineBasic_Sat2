0a3f0e1bc4fc118e411e302426836f39e4b4d7a7
qnpfr307 <100455847+good0neighbour@users.noreply.github.com>
2022-12-24 18:40:28 +0900

    RPG3D
    Scripts created:
    - AnimationWrapper
    - - Controls animation status.
    - CharacterStateBase
    - - An abstract class for chatarcter states.
    - CharacterStateMachine
    - - Controls character states.
    - PlayerStateMachine
    - - Just created.
    - PlayerStateMove
    - - Just created.
    - IState
    - - An interface for character states.
    Animation created:
    - 8 derections transition.

---
+++ RPG3D/Assets/02.Scripts/StateMachine/AnimatorWrapper.cs
@@ -0,0 +1,15 @@
+﻿using TMPro.EditorUtilities;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimatorWrapper : MonoBehaviour
+{
+    public bool isPreviousStateFinished { get; }
+    public bool isPreviousMachineFinished { get; }
+    [SerializeField] private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+}
---
+++ RPG3D/Assets/02.Scripts/StateMachine/Expansions_Player/PlayerStateMove.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMove : CharacterStateBase
+{
+    public PlayerStateMove(int id, GameObject owner, Func<bool> executeCondition) : base(id, owner, executeCondition)
+    {
+    }
+}
